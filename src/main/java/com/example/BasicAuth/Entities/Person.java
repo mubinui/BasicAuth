4aeab45e1656bd445dc1d5cc643c552d0dbd2c17
mubinui <mubin41@student.sust.edu>
2022-10-17 13:12:20 +0600

    Basic Authentication without database

---
+++ src/main/java/com/example/BasicAuth/Entities/Person.java
@@ -0,0 +1,68 @@
+package com.example.BasicAuth.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false)
+    private String email;
+    private String username;
+    @Column(nullable = false)
+    private String password ;
+
+    public Person(Long id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/BasicAuth/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.BasicAuth.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.
+                csrf().disable().
+                authorizeRequests().
+                anyRequest().
+                authenticated().and().
+                httpBasic();
+    }
+    //
+
+
+
+}
+
